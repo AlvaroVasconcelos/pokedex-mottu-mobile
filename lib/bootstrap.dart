1b5685f5f5e3001f877deb33bf37edd7ed0e1bba
Vasconcelos.dev <vasconcelos.dev@outlook.com>
2025-09-25 17:57:01 -0300

    feat(bootstrap): enhance HTTP client setup with caching interceptor

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
@@ -12,15 +13,22 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
-
+  final httpClient = Dio(
+    BaseOptions(
+      baseUrl: 'https://pokeapi.co/api/v2/',
+    ),
+  );
+  httpClient.interceptors.add(LogInterceptor(responseBody: false));
+  final options = CacheOptions(
+    store: MemCacheStore(),
+    policy: CachePolicy.forceCache,
+    priority: CachePriority.normal,
+    maxStale: const Duration(days: 7),
+    allowPostMethod: true,
+  );
+  httpClient.interceptors.add(DioCacheInterceptor(options: options));
   Get
-    ..put<Dio>(
-      Dio(
-        BaseOptions(
-          baseUrl: 'https://pokeapi.co/api/v2/',
-        ),
-      ),
-    )
+    ..put<Dio>(httpClient)
     ..put<IPokemonRepository>(PokemonRepository(Get.find<Dio>()));
   runApp(await builder());
 }
